f3adc039b8eba809fca04b8fcf2176b4e1c12897
Nabeel Sarwar <nabeelsarwar200@gmail.com>
2016-03-26 21:26:13 -0400

    updating remove error big match for future analysis

--- matching/RemoveErrorBigMatch.py
+++ matching/RemoveErrorBigMatch.py
@@ -22,7 +22,6 @@ goodIndices = sand(goodIndices, matchedCat['cmodel_flux_err_z'] != 0)
 goodIndices = sand(goodIndices, matchedCat['cmodel_flux_err_y'] != 0)
 
 #use this matchedCat for the proper compilation of matches
-matchedCat = matchedCat[goodIndices]
 magG = -2.5*np.log10(matchedCat['cmodel_flux_g']/matchedCat['flux_zeromag_g'])
 magR = -2.5*np.log10(matchedCat['cmodel_flux_r']/matchedCat['flux_zeromag_r'])
 magI = -2.5*np.log10(matchedCat['cmodel_flux_i']/matchedCat['flux_zeromag_i'])
@@ -73,16 +72,16 @@ orig_cols = matchedCat.columns
 orig_cols = pyfits.ColDefs([pyfits.Column(name = col.name, format=col.format, array=np.array(col.array)[goodIndices]) for col in orig_cols])
 new_cols = pyfits.ColDefs(cols)
 hdu = pyfits.BinTableHDU.from_columns(orig_cols + new_cols)
-os.remove('MatchHellErrorCut.fits')
-hdu.writeto('MatchHellErrorCut.fits')
+os.remove('MatchHellErrorCut2.fits')
+hdu.writeto('MatchHellErrorCut2.fits')
 
 #dirty way to not have to deal with the data
-matchedCat = pyfits.getdata('MatchHellErrorCut.fits')
+matchedCat = pyfits.getdata('MatchHellErrorCut2.fits')
 
 # establish the indices with proper colors, and then get this
 
 #possible errors ahead
-goodIndices = magRError < 0
+goodIndices = magRError < 0.2
 goodIndices = sand(goodIndices, magIError < 0.2)
 goodIndices = sand(goodIndices, magZError < 0.2)
 goodIndices = sand(goodIndices, magGError < 0.2)
