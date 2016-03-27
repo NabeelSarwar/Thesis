ea0d8aca328c5943685dca09979a13d4d904a561
Nabeel Sarwar <nabeelsarwar200@gmail.com>
2016-03-26 21:37:33 -0400

    added clustering to use not noisy data

--- classification/Clustering.py
+++ classification/Clustering.py
@@ -117,7 +117,7 @@ def tryModel(model, data, ids, results):
     badGalaxyIDs = ids[badGalaxyIndices]
     return {'training': trainingReport, 'test': testReport}, badStarIDs, badGalaxyIDs
 
-matchedCat = pyfits.getdata('MatchHell2.fits')
+matchedCat = pyfits.getdata('MatchHellErrorCut2.fits')
 #shortcut and
 sand = np.logical_and
 
